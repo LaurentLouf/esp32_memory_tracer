6c2678879cdfc4c4a86979e82999556541627283
Laurent Louf <laurent@getmoona.com>
2019-06-25 11:04:00 +0200

    Add the possibility to switch between addr2line and gdb in the backtrace decoder. See https://github.com/me-no-dev/EspExceptionDecoder/issues/23

--- backtrace_decoder.py
+++ backtrace_decoder.py
@@ -7,16 +7,17 @@ import sys
 import pexpect
 
 
-def print_call_stack_info(call_stack, symbol_file, remove_from_path):
+def print_call_stack_info(call_stack, symbol_file, remove_from_path, tool):
     # Print the call stack associated with this heap trace
     for address in call_stack:
         if address != "0x40000000":
-            addr2line = pexpect.spawn("xtensa-esp32-elf-addr2line -pfiaC -e " + symbol_file + " " +
-                                      address)
-            addr2line.logfile = None
-            match = addr2line.expect([pexpect.EOF, pexpect.TIMEOUT], timeout=2)
-            if match == 0:
-                print(addr2line.before.strip().replace(remove_from_path, ""))
+            if tool == "addr2line":
+                addr2line = pexpect.spawn("xtensa-esp32-elf-addr2line -pfiaC -e " + symbol_file + " " +
+                                        address)
+                addr2line.logfile = None
+                match = addr2line.expect([pexpect.EOF, pexpect.TIMEOUT], timeout=2)
+                if match == 0:
+                    print(addr2line.before.strip().replace(remove_from_path, ""))
         else:
             print("0x40000000: top of call stack reached")
 
@@ -36,6 +37,10 @@ if __name__ == "__main__":
         "--remove_from_path",
         help="Remove a string from the paths displayed for a more concise display",
         default="")
+    parser.add_argument(
+        "--tool",
+        help="Specify the tool to use for address decoding",
+        choices=['addr2line', 'gdb'], default='gdb')
     args = parser.parse_args()
 
     if args.backtrace is not None and args.symbol_file is not None:
@@ -47,4 +52,4 @@ if __name__ == "__main__":
             sys.exit(-1)
 
         backtrace = [address[0:address.find(":")] for address in reversed(args.backtrace[1:])]
-        print_call_stack_info(backtrace, symbol_file, args.remove_from_path)
+        print_call_stack_info(backtrace, symbol_file, args.remove_from_path, args.tool)
