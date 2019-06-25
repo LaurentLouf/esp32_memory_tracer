99ac0d4f45971bda2f53912726d0edc58906e07e
Laurent Louf <laurent@getmoona.com>
2019-06-25 11:12:03 +0200

    Formatting

--- backtrace_decoder.py
+++ backtrace_decoder.py
@@ -8,26 +8,32 @@ import pexpect
 import colorama
 import re
 
+
 def print_call_stack_info(call_stack, symbol_file, remove_from_path, tool):
     # Print the call stack associated with this heap trace
     for address in call_stack:
         if address != "0x40000000":
             if tool == "addr2line":
-                addr2line = pexpect.spawn("xtensa-esp32-elf-addr2line -pfiaC -e " + symbol_file + " " +
-                                        address)
+                addr2line = pexpect.spawn("xtensa-esp32-elf-addr2line -pfiaC -e " + symbol_file + " " + address)
                 addr2line.logfile = None
                 match = addr2line.expect([pexpect.EOF, pexpect.TIMEOUT], timeout=2)
                 if match == 0:
                     print(addr2line.before.strip().replace(remove_from_path, ""))
             elif tool == "gdb":
-                gdb = pexpect.spawn("xtensa-esp32-elf-gdb --batch " + symbol_file + " -ex \"set listsize 1\" -ex \"l *" + address + "\"")
+                gdb = pexpect.spawn("xtensa-esp32-elf-gdb --batch " + symbol_file +
+                                    " -ex \"set listsize 1\" -ex \"l *" + address + "\"")
                 gdb.logfile = None
                 match = gdb.expect([pexpect.EOF, pexpect.TIMEOUT], timeout=2)
                 if match == 0:
                     output_filtered = gdb.before.strip().replace(remove_from_path, "")
-                    components = re.match(r".*(?P<address>0x[0-9a-z]+) is in (?P<function>\S+) \((?P<file>.*):(?P<line>[0-9]+)\)", output_filtered)
+                    components = re.match(
+                        r".*(?P<address>0x[0-9a-z]+) is in (?P<function>\S+) \((?P<file>.*):(?P<line>[0-9]+)\)",
+                        output_filtered)
                     if components is not None:
-                        print(colorama.Style.RESET_ALL + colorama.Fore.RED + components.group('address') + colorama.Style.RESET_ALL + " : " + colorama.Fore.GREEN + components.group('function') + colorama.Style.RESET_ALL + " in " + colorama.Fore.BLUE + components.group('file') + ":" + colorama.Style.BRIGHT + components.group('line'))
+                        print(colorama.Style.RESET_ALL + colorama.Fore.RED + components.group('address') +
+                              colorama.Style.RESET_ALL + " : " + colorama.Fore.GREEN + components.group('function') +
+                              colorama.Style.RESET_ALL + " in " + colorama.Fore.BLUE + components.group('file') + ":" +
+                              colorama.Style.BRIGHT + components.group('line'))
         else:
             print("0x40000000: top of call stack reached")
 
@@ -46,21 +52,15 @@ if __name__ == "__main__":
         type=open)
     parser.add_argument("backtrace", help="Backtrace printed by the ESP32", nargs='*')
     parser.add_argument(
-        "--remove_from_path",
-        help="Remove a string from the paths displayed for a more concise display",
-        default="")
+        "--remove_from_path", help="Remove a string from the paths displayed for a more concise display", default="")
     parser.add_argument(
-        "--tool",
-        help="Specify the tool to use for address decoding",
-        choices=['addr2line', 'gdb'], default='gdb')
+        "--tool", help="Specify the tool to use for address decoding", choices=['addr2line', 'gdb'], default='gdb')
     args = parser.parse_args()
 
     if args.backtrace is not None and args.symbol_file is not None:
         symbol_file = args.symbol_file.name
         if args.backtrace[0] != "Backtrace:":
-            print(
-                "Incorrect format detected, expected to receive a string beginning with \"Backtrace: \""
-            )
+            print("Incorrect format detected, expected to receive a string beginning with \"Backtrace: \"")
             sys.exit(-1)
 
         backtrace = [address[0:address.find(":")] for address in reversed(args.backtrace[1:])]
